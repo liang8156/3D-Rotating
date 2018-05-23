b25f7879022474e3d8c0d442c0f13192be3e28b0
alexLiang <liang8156@gmail.com>
2018-05-24 00:55:31 +0800

    註解更新

--- scripts/index.js
+++ scripts/index.js
@@ -13,21 +13,21 @@ function run(index) {
     if (index === '3') { x -= 90; }
     if (index === '4') { x = 0; y = 0; ul.style.webkitTransition = '-webkit-transform 0.1s linear'; }    //當點擊重置按鈕時，迅速轉回到初始狀態。
     ul.style.webkitTransform = "rotateX(" + x + "deg) rotateY(" + y + "deg)";    //變換效果（沿X軸和Y軸旋轉）
-    console.warn(x, y)
+    console.warn(x)
 }
 ////////////////////////鍵盤控制    
 document.addEventListener('keydown', function (e) {
     ul.style.webkitTransition = '-webkit-transform 3s linear';
     switch (e.keyCode) {
-        case 37: y -= 90;    //左箭頭
+        case 37: y -= 90;    //左
             break;
-        case 38: x += 90;    //上箭頭
+        case 38: x += 90;    //上
             break;
-        case 39: y += 90;    //下箭頭
+        case 39: y += 90;    //右
             break;
-        case 40: x -= 90;    //右箭頭
+        case 40: x -= 90;    //下
             break;
-        case 13: x = 0; y = 0;    //回車 （當回車時，迅速轉回初始狀態）
+        case 13: x = 0; y = 0;    //迅速轉回初始狀態
             ul.style.webkitTransition = '-webkit-transform 0.1s linear';
             break;
     }
