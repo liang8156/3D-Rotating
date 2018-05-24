379473c111b7cbd44b475575d14fa1dad4313ac7
alexLiang <liang8156@gmail.com>
2018-05-24 21:54:55 +0800

    加入滑鼠事件使方塊轉動

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,6 @@ function run(index) {
     if (index === '3') { x -= 90; }
     if (index === '4') { x = 0; y = 0; ul.style.webkitTransition = '-webkit-transform 0.1s linear'; }    //當點擊重置按鈕時，迅速轉回到初始狀態。
     ul.style.webkitTransform = "rotateX(" + x + "deg) rotateY(" + y + "deg)";    //變換效果（沿X軸和Y軸旋轉）
-    console.warn(x)
 }
 ////////////////////////鍵盤控制    
 document.addEventListener('keydown', function (e) {
@@ -33,3 +32,32 @@ document.addEventListener('keydown', function (e) {
     }
     ul.style.webkitTransform = "rotateX(" + x + "deg) rotateY(" + y + "deg)"; //變換效果（沿X軸和Y軸旋轉）
 }, false);
+
+function down(e) {
+    var e = e || window.event;
+    x1 = e.clientX;
+    y1 = e.clientY;
+    document.onmousemove = move;
+    document.onmouseup = up;
+    console.log("x1,y1", x1, y1)
+}
+function move(e) {
+    document.getElementById('div1').innerHTML = '';
+    var e = e || window.event;
+    var x2 = e.clientX;
+    var y2 = e.clientY;
+    //console.log("x2,y2", x2, y2)
+    ul.style.webkitTransition = '-webkit-transform 0s linear';
+    x += (x2 - x1) * 0.5;
+    y += (y2 - y1) * 0.5;
+    console.log("x,y", x, y)
+    ul.style.webkitTransform = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+
+}
+function up() {
+    document.onmousemove = null;
+    document.onmouseup = null;
+}
+document.onmousedown = down;
+
+
